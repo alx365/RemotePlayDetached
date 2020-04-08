e19c18345550a92c46a0e4fec7abc724e9bae80b
Alexander Reinhardt <30902201+alx365@users.noreply.github.com>
2020-04-08 07:37:01 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo This script will deinstall everything just like it would be installed in the makepkg file. If you changed something here, also change it here. You had 4 seconds to press ctrl+c in order to stop this script
-wait 4
+sleep 4
 sudo rm /usr/bin/RemotePlayDetached
 rm ~/.local/share/applications/remoteplaydetached.desktop
 rm ~/.icons/remoteplaydetached.png
